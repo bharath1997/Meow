fea8ef9cc19f4ed64e7b38c4dabef24d47b140dd
Bharath Venkatesan <bharath.venkatesan@openbet.com>
2022-11-02 11:57:45 +0530

    change in cats db

--- app/src/main/java/com/example/meow/util/Constants.kt
+++ app/src/main/java/com/example/meow/util/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val API_KEY = "live_AGLSMHMtSZ0cnccqvqQGIPKdyPLB8g6uOVfRTkyCDEpOr7dXUc6PCBj6Qeo9hRzt"
     const val API_KEY_NAME = "x-api-key"
     const val BASE_API = "https://api.thecatapi.com/"
-    const val DATABASE_NAME = "https://api.thecatapi.com/"
+    const val DATABASE_NAME = "catsdb.db"
     const val QUERY_PAGE_SIZE = 30
 }
